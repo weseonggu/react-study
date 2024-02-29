f4a903b27bb88f313ee72cf74afa003503516ac8
DESKTOP-N8PDVAS\WeSeongGu <fkskdldh@naver.com>
2024-02-29 18:40:56 +0900

    컨텍스트를 사용한 로그인 여부 확인

---
+++ src/components/todo/security/AuthContext.js
@@ -0,0 +1,25 @@
+import { createContext, useContext, useState } from "react";
+
+//1: Create a Context
+export const AuthContext = createContext()
+
+export const useAuth = () => useContext(AuthContext)
+
+//2: Share the created context with other components
+export default function AuthProvider({ children }) {
+
+    //Put some state in the context
+    const [number, setNumber] = useState(10)
+
+    const [isAuthenticated, setAuthenticated] = useState(false)
+
+    // setInterval( () => setNumber(number+1), 10000)
+
+    //const valueToBeShared = {number, isAuthenticated, setAuthenticated}
+
+    return (
+        <AuthContext.Provider value={ {number, isAuthenticated, setAuthenticated}  }>
+            {children}
+        </AuthContext.Provider>
+    )
+} 
